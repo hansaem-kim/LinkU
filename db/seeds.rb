875b30e7d7957f381c0aed1a8499fb5639925b17
Hansaem Kim <hansaem_kim@hotmail.com>
2021-09-16 11:52:37 -0400

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Post.destroy_all
+About.destroy_all
+Experience.destroy_all
+Education.destroy_all
+
+demoUser = User.create!(
+    email: "test1@gmail.com",
+    password: 'hansaem',
+    first_name: 'Hansaem',
+    last_name: 'Kim',
+)
+
