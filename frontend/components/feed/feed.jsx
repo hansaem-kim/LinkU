3dba39d8fc3359600e77e7cfe7d4d4f51993e509
Hansaem Kim <hansaem_kim@hotmail.com>
2021-10-19 12:27:16 +0900

    Add Mini Profile

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -5,9 +5,12 @@ class Feed extends React.Component{
 
     componentDidMount(){
         this.props.getUsers();
+        this.props.fetchConnections(this.props.currentUser.id)
     }
 
     render(){
+        const userPic = this.props.currentUser.profilePic ? <img src={this.props.currentUser.profilePic} /> : <img src={window.profileDefault} />
+
         return(
             <div className='feed-div'>
                 <div className='post-div'>
@@ -42,25 +45,41 @@ class Feed extends React.Component{
                 </div>
                 <PostIndexContainer />
 
+                <div className='user-mini-info'>
+                    <header>
+                        {userPic}
+                        <h1>{this.props.currentUser.first_name} {this.props.currentUser.last_name}</h1>
+                        <p>{this.props.currentUser.headline}</p>
+                    </header>
+                    <div className='fololwer-num'>
+                        {this.props.connections.length} followers
+                    </div>
+                    
+
+
+
+
+                </div>
+
                 <div className='mini-profile-div'>
                     <img className = 'hansaem-photo' src={window.hansaemPhoto} />
                     <h1>Hello, I'm Hansaem.</h1>
                     <p>Please feel free to play around with LinkU</p>
-                    <div class='sites'>
+                    <div className='sites'>
                         <a href="https://hansaem-kim.github.io/" target="_blank">
-                        <i class="fas fa-user-circle fa-3x"></i>
+                            <i className="fas fa-user-circle fa-3x"></i>
                             <p>Portfolio</p>
                         </a>
                         <a href="https://www.linkedin.com/in/hansaem-kim-244238127/" target="_blank">
-                            <i class="fab fa-linkedin fa-3x"></i>
+                            <i className="fab fa-linkedin fa-3x"></i>
                             <p>LinkedIn</p>
                         </a>
                         <a href="https://github.com/hansaem-kim" target="_blank">
-                            <i class="fab fa-github fa-3x"></i>
+                            <i className="fab fa-github fa-3x"></i>
                             <p>Github</p>
                         </a>
                         <a href="https://angel.co/u/hansaem-kim" target="_blank">
-                            <i class="fab fa-angellist fa-3x"></i>
+                            <i className="fab fa-angellist fa-3x"></i>
                             <p>AngelList</p>
                         </a>
                     </div>
--- frontend/components/feed/feed_container.jsx
+++ frontend/components/feed/feed_container.jsx
@@ -1,23 +1,29 @@
 import { connect } from "react-redux";
-import React from "react";
 import { showModal } from "../../actions/modal_actions";
 import { fetchAllPosts } from "../../actions/post_actions";
 import { getUsers } from '../../actions/user_actions';
+import { fetchConnections } from '../../actions/connection_actions';
+
 import Feed from './feed'
 
 const mSTP = (state) => {
     const posts = state.entities.posts;
     const currentUser = state.entities.users[state.session.id];
+    
     return ({
         posts,
-        currentUser
+        currentUser,
+        connections: Object.values(state.entities.connections)
+
     })
 };
 
 const mDTP = (dispatch) => ({
     fetchAllPosts: () => dispatch(fetchAllPosts()),
     getUsers: () => dispatch(getUsers()),
-    showModal: (modal) => dispatch(showModal(modal))
+    showModal: (modal) => dispatch(showModal(modal)),
+    fetchConnections: (userId) => dispatch(fetchConnections(userId)),
+
 
 });
 
